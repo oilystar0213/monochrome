d46a30f4d171bef69efbcaa63a6a3027f4023bc1
oilystar0213 <oilystar0213@gmail.com>
2025-09-17 23:07:58 +0900

    commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,17 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController controller;
     public Transform cam;
 
-    public float speed = 5f;
+    public float speed = 10f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    Animator anim;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -22,14 +25,17 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if (direction.magnitude >= 0.1f)
+        bool isMoving = direction.magnitude >= 0.1f;
+        anim.SetBool("isRunning", isMoving);
+
+        if (isMoving)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir * speed * Time.deltaTime);
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
 }
